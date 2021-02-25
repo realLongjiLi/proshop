dedce8b7278864b47b2c86f34f68fc91f8821253
longjil2 <sdszllj@gmail.com>
2021-02-25 11:37:03 +0800

    created backend, linked the home screen to backend

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
-import products from '../products'
+import axios from 'axios'
 
 const HomeScreen = () => {
+    // make products part of the component
+    const [products, setProducts] = useState([])
+
+    // run it as soon as the component loads
+    useEffect(() => {
+        const fetchProducts = async () => {
+            const { data } = await axios.get('/api/products')
+            
+            setProducts(data)
+        }
+        fetchProducts()
+    }, [])
     return (
         <>
             <h1>Latest Products</h1>
