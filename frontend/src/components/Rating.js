4373cb884f861f54318917057b15e6d84ca633a2
longjil2 <sdszllj@gmail.com>
2021-02-25 13:02:58 +0800

    linked product screen to backend

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -60,8 +60,8 @@ const Rating = ({ value, text }) => {
 }
 
 Rating.propTypes = {
-    value: PropTypes.number.isRequired,
-    text: PropTypes.string.isRequired,
+    value: PropTypes.number,
+    text: PropTypes.string,
 }
 
 export default Rating
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
 import Rating from '../components/Rating'
-import products from '../products'
+
+import axios from 'axios'
 
 const ProductScreen = ({ match }) => {
-    const product = products.find(p => p._id === match.params.id)
+    // make products part of the component
+    const [product, setProduct] = useState({})
+
+    // run it as soon as the component loads
+    useEffect(() => {
+        const fetchProduct = async () => {
+            const { data } = await axios.get(`/api/products/${match.params.id}`)
+            
+            setProduct(data)
+        }
+        fetchProduct()
+    }, [match.params.id])
+
     return (
         <>
         <Link className='btn btn-light my-3' to='/'>Go Back</Link>
@@ -19,13 +32,13 @@ const ProductScreen = ({ match }) => {
                         <h3>{product.name}</h3>
                     </ListGroup.Item>
                     <ListGroup.Item>
-                        <Rating value={product.rating} text = {` ${product.numReviews} reviews`}></Rating>
+                        <Rating value={product.rating} text={` ${product.numReviews} reviews`}></Rating>
                     </ListGroup.Item>
                     <ListGroup.Item>
                         Price: ${product.price}
                     </ListGroup.Item>
                     <ListGroup.Item>
-                        Description: ${product.description}
+                        Description: {product.description}
                     </ListGroup.Item>
                 </ListGroup>
             </Col>
