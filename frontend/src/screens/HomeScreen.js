870dc055dfbdf36a06ff7d27e651a58255ea5513
longjil2 <sdszllj@gmail.com>
2021-03-05 20:24:47 +0800

    Implemented redux for state management

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,31 +1,40 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
-import axios from 'axios'
+import { listProducts } from '../actions/productActions'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
 
 const HomeScreen = () => {
-    // make products part of the component
-    const [products, setProducts] = useState([])
+    const dispatch = useDispatch()
+
+    const productList = useSelector(state => state.productList)
+    const { loading, error, products} = productList
 
     // run it as soon as the component loads
     useEffect(() => {
-        const fetchProducts = async () => {
-            const { data } = await axios.get('/api/products')
-            
-            setProducts(data)
-        }
-        fetchProducts()
-    }, [])
+        dispatch(listProducts())
+    }, [dispatch])
+
+    // loading, error, and else
     return (
         <>
             <h1>Latest Products</h1>
-            <Row>
-                {products.map(product => (
-                    <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                        <Product product={product}></Product>
-                    </Col>
-                ))}
-            </Row>
+            {   loading ? (
+                  <Loader></Loader>
+                ) : error ? (
+                  <Message variant='danger'>{error}</Message>
+                ) : (
+                <Row>
+                    {products.map(product => (
+                        <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                            <Product product={product}></Product>
+                        </Col>
+                    ))}
+                </Row>
+                )
+            }
         </>
     )
 }
