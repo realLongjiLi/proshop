37719d9721ce7cc81bdb8aad037845554963cbce
longjil2 <sdszllj@gmail.com>
2021-03-06 11:27:15 +0800

    Added quantity selector for products and initialized cart screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,21 @@
 import React from 'react'
-import { BrowserRouter as Router, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
+import CartScreen from './screens/CartScreen'
 
 const App = () => {
   return (
     <Router>
       <Header></Header>
       <main className='py-3'>
-        <Container> 
+        <Container>
           <Route path='/' component={HomeScreen} exact></Route>
           <Route path='/product/:id' component={ProductScreen}></Route>
+          <Route path='/cart/:id?' component={CartScreen}></Route>
         </Container>
       </main>
       <Footer></Footer>
@@ -21,4 +23,4 @@ const App = () => {
   )
 }
 
-export default App;
+export default App
