179ba5f76dea2d5e645ec4035ee7b02a09400fcb
longjil2 <sdszllj@gmail.com>
2021-03-09 15:35:17 +0800

    Implemented update user profile (backend)

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,19 +4,17 @@ import {
   authUser,
   getUserProfile,
   registerUser,
+  updateUserProfile,
 } from '../controllers/userController.js'
 import { protect } from '../middleware/authMiddleware.js'
 
 router.route('/').post(registerUser)
 
-// @desc    Auth user & get token
-// @route   POST /api/users/login
-// @access  Public
 router.post('/login', authUser)
 
-// @desc    Get user profile
-// @route   GET /api/users/profile
-// @access  Private
-router.route('/profile').get(protect, getUserProfile)
+router
+  .route('/profile')
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile)
 
 export default router
